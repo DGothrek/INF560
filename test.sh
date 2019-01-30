b620a964320fc270a29bfbe01058181cb4b22629
Mael MADON <mael.madon@polytechnique.edu>
2019-01-30 10:25:14 +0100

    before checkout

--- test.sh
+++ test.sh
@@ -6,8 +6,8 @@ INPUT_DIR=images/original
 OUTPUT_DIR=images/processed
 mkdir $OUTPUT_DIR 2>/dev/null
 
-i=images/original/australian-flag-large.gif
-DEST=images/processed/australian-flag-large.gif
+i=images/original/Mandelbrot-large.gif
+DEST=images/processed/Mandelbrot-large.gif
 echo "Running test on $i -> $DEST"
 
 salloc -N 1 -n 2 mpirun ./sobelf $i $DEST
